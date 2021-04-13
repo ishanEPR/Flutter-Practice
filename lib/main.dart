141c8c3cd0657c74cbe0998adc73406f07e01062
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-04-13 09:45:54 +0530

    create container class attribute

--- lib/main.dart
+++ lib/main.dart
@@ -48,71 +48,131 @@
 //   }
 // }
 
+// import 'package:flutter/material.dart';
+//
+// void main(){
+//   runApp(MyApp());
+//
+// }
+//
+// class MyApp extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: HomePage(),
+//     );
+//   }
+//
+// }
+//
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return DefaultTabController(
+//       length: 3,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Flutter'),
+//           leading: IconButton(
+//             icon: Icon(Icons.menu),
+//             onPressed: (){},
+//           ),
+//           actions: [
+//             IconButton(icon: Icon(Icons.search), onPressed:(){}),
+//             IconButton(icon: Icon(Icons.more_vert), onPressed:(){}),
+//           ],
+//           //flexibleSpace: Image.asset("assests/ishan.png",fit: BoxFit.cover,),
+//           bottom:TabBar(
+//             tabs: [
+//               Tab(icon: Icon(Icons.directions_car),text: "Car",),
+//               Tab(icon: Icon(Icons.directions_train),),
+//               Tab(icon: Icon(Icons.directions_bike),),
+//             ],
+//           ),
+//           elevation: 22.0,
+//           backgroundColor: Colors.blue,
+//         ),
+//         body: TabBarView(
+//           children: [
+//            tab1(),
+//             Icon(Icons.directions_train),
+//             Icon(Icons.directions_bike),
+//           ],
+//         ),
+//
+//       ),
+//     );
+//   }
+// }
+// Widget tab1(){
+//   return Container(
+//     child: Center(
+//       child: Text('Ishan Reshmika'),
+//     ),
+//   );
+//
+// }
+
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(MyApp());
-
 }
-
-class MyApp extends StatelessWidget{
+ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
+   return MaterialApp(
+     debugShowCheckedModeBanner: false,
+     theme: ThemeData(
+       brightness: Brightness.light,
+       primarySwatch: Colors.orange,
+
+
+     ),
+     home: MyHomePage(),
+   );
   }
 
-}
+ }
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter'),
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: (){},
-          ),
-          actions: [
-            IconButton(icon: Icon(Icons.search), onPressed:(){}),
-            IconButton(icon: Icon(Icons.more_vert), onPressed:(){}),
-          ],
-          //flexibleSpace: Image.asset("assests/ishan.png",fit: BoxFit.cover,),
-          bottom:TabBar(
-            tabs: [
-              Tab(icon: Icon(Icons.directions_car),text: "Car",),
-              Tab(icon: Icon(Icons.directions_train),),
-              Tab(icon: Icon(Icons.directions_bike),),
-            ],
-          ),
-          elevation: 22.0,
-          backgroundColor: Colors.blue,
-        ),
-        body: TabBarView(
-          children: [
-           tab1(),
-            Icon(Icons.directions_train),
-            Icon(Icons.directions_bike),
-          ],
-        ),
+ class MyHomePage extends StatelessWidget {
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+       appBar: AppBar(
+         title:Text("Hello Ishan"),
+       ),
+       body: Container(
+        // height: 300.0,
+         margin: EdgeInsets.only(top: 10.0,left: 20.0,right: 20.0,bottom: 10.0),
+       //  padding: EdgeInsets.all(20.0),
+         padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 40.0),
+         transform: Matrix4.rotationZ(0.1),
+         alignment: Alignment.bottomLeft,
+         width: double.infinity,
+         color: Colors.pinkAccent,
+         child: Text("Hello Ishan",style: TextStyle(fontSize: 40.0),),
+       ),
 
-      ),
-    );
-  }
-}
-Widget tab1(){
-  return Container(
-    child: Center(
-      child: Text('Ishan Reshmika'),
-    ),
-  );
+     );
+   }
+ }
+ 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
