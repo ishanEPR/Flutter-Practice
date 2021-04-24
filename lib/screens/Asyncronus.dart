1db08ef25fb20ef9da8589554414c91f62639bca
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-04-24 09:21:21 +0530

    Asyncronsus programming added

--- lib/screens/Asyncronus.dart
+++ lib/screens/Asyncronus.dart
@@ -5,14 +5,17 @@ class Async_page extends StatefulWidget {
 }
 
 class _Async_pageState extends State<Async_page> {
-  void getData(){
-    Future.delayed(Duration(seconds: 3),(){
-      print('ishan@gmail.com');
+  void getData() async {
+   String email=await Future.delayed(Duration(seconds: 3),(){
+      //print('ishan@gmail.com');
+     return "ishan@gmail.com";
     });
-    print("Hello Ishan");
-    Future.delayed(Duration(seconds: 2),(){
-      print('name:Ishan,age:23');
+  //  print("Hello Ishan");
+   String name=await Future.delayed(Duration(seconds: 2),(){
+     // print('name:Ishan,age:23');
+     return "name:Ishan,age:23";
     });
+   print(email+" "+name);
   }
 
   @override
